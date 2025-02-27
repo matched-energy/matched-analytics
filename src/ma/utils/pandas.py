caa31219f03a9cc645ef3d0f8cab9d777a03235c
Benedict Shegog <benedict.shegog@gmail.com>
2025-02-27 18:05:22 +0000

    Fix type errors in pandas utils and schema files

--- src/ma/utils/pandas.py
+++ src/ma/utils/pandas.py
@@ -3,14 +3,15 @@ from typing import Callable, Dict, NotRequired, Optional, TypedDict, Union
 
 import pandas as pd
 import pandera as pa
+from pandera.engines.pandas_engine import DateTime
 
 
 def select_columns(df: pd.DataFrame, exclude: list) -> pd.DataFrame:
     return df[[col for col in df.columns if col not in exclude]]
 
 
-def DateTimeEngine(dayfirst: bool = True) -> pa.Column:
-    return pa.engines.pandas_engine.DateTime({"dayfirst": dayfirst})
+def DateTimeEngine(dayfirst: bool = True) -> DateTime:
+    return DateTime({"dayfirst": dayfirst})
 
 
 class ColumnSchema(TypedDict):
