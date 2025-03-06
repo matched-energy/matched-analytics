4c35c89eba10d0e3d2c44ac0ebaf360f2ecb9646
Joe Kwiatkowski <joejk2@gmail.com>
2025-03-06 11:33:01 +0000

    Add metering_data_rollup, and associated changes

---
+++ tests/ma/elexon/test_metering_data_rollup.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import pytest
+
+import data.register
+from ma.elexon.metering_data import load_file
+from ma.elexon.metering_data_rollup import rollup_from_daily, rollup_to_daily
+from ma.utils.enums import TemporalGranularity
+
+
+def test_rollup_to_daily() -> None:
+    metering_data_half_hourly = load_file(data.register.S0142_20230330_SF_20230425121906_GOLD_CSV)
+    metering_data_daily = rollup_to_daily(metering_data_half_hourly)
+
+    assert isinstance(metering_data_daily.index, pd.DatetimeIndex)
+    assert len(metering_data_daily) == 1
+    assert metering_data_daily["bm_unit_metered_volume_mwh"].iloc[0] == -3417.849
+    assert metering_data_daily["settlement_period_count"].iloc[0] == 48
+
+
+def test_rollup_to_daily_multiple_days() -> None:
+    date1 = pd.Timestamp("2025-03-01")
+    date2 = pd.Timestamp("2025-03-02")
+    times = pd.date_range(date1, periods=24, freq="30T").append(pd.date_range(date2, periods=24, freq="30T"))
+    data = pd.DataFrame({"value": range(48)}, index=times)
+
+    with pytest.raises(AssertionError, match="Data should not span days"):
+        rollup_to_daily(data)
+
+
+def test_rollup_from_daily_monthly() -> None:
+    metering_data_daily_dataframes = [
+        rollup_to_daily(load_file(half_hourly))
+        for half_hourly in [
+            data.register.S0142_20230330_SF_20230425121906_GOLD_CSV,
+            data.register.S0142_20230331_SF_20230426191253_GOLD_CSV,
+        ]
+    ]
+    metering_data_monthly = rollup_from_daily(metering_data_daily_dataframes, TemporalGranularity.MONTHLY)
+
+    assert len(metering_data_monthly) == 1
+    assert metering_data_monthly.loc["2023-03-01", "settlement_period_count"] == 96
+    assert metering_data_monthly.loc["2023-03-01", "day_count"] == 2
+    assert metering_data_monthly.loc["2023-03-01", "bm_unit_metered_volume_mwh"] == -6945.599
+
+
+def test_rollup_from_daily_yearly() -> None:
+    metering_data_daily_dataframes = [
+        rollup_to_daily(load_file(half_hourly))
+        for half_hourly in [
+            data.register.S0142_20230330_SF_20230425121906_GOLD_CSV,
+            data.register.S0142_20230331_SF_20230426191253_GOLD_CSV,
+        ]
+    ]
+    metering_data_monthly = rollup_from_daily(metering_data_daily_dataframes, TemporalGranularity.MONTHLY)
+    metering_data_yearly = rollup_from_daily([metering_data_monthly], TemporalGranularity.YEARLY)
+
+    assert len(metering_data_yearly) == 1
+    assert metering_data_yearly.loc["2023-01-01", "settlement_period_count"] == 96
+    assert metering_data_yearly.loc["2023-01-01", "day_count"] == 2
+    assert metering_data_yearly.loc["2023-01-01", "month_count"] == 1
+    assert metering_data_monthly.loc["2023-03-01", "bm_unit_metered_volume_mwh"] == -6945.599
+
+
+def test_rollup_from_daily_empty_list() -> None:
+    with pytest.raises(AssertionError, match="Input list must not be empty"):
+        rollup_from_daily([], TemporalGranularity.MONTHLY)
