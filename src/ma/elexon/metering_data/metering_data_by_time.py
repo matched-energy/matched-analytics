0d43c29d885e52e438f0ab9da35570b21a63fddb
Joe Kwiatkowski <joejk2@gmail.com>
2025-03-11 11:06:52 +0000

    Data asset operations are now classmethods

--- src/ma/elexon/metering_data/metering_data_by_time.py
+++ src/ma/elexon/metering_data/metering_data_by_time.py
@@ -32,12 +32,58 @@ class MeteringDataHalfHourly(DataFrameAsset):
     }
     # fmt: on
 
+    @classmethod
+    def transform_to_daily(
+        cls,
+        metering_data_half_hourly: MeteringDataHalfHourlyType,
+    ) -> MeteringDataDailyType:
+        """Rollup a single, half-hourly dataframe to a daily dataframe"""
+        assert isinstance(metering_data_half_hourly.index, pd.DatetimeIndex)  # appease mypy
+        days = metering_data_half_hourly.index.to_period("D")
+        assert len(days.unique()) == 1, "Data should not span days"
 
-class MeteringDataDaily(MeteringDataHalfHourly):
+        daily_total = (
+            metering_data_half_hourly[
+                [
+                    "period_bm_unit_balancing_services_volume",
+                    "period_information_imbalance_volume",
+                    "period_expected_metered_volume",
+                    "bm_unit_metered_volume_mwh",
+                    "bm_unit_applicable_balancing_services_volume",
+                    "period_supplier_bm_unit_delivered_volume",
+                    "period_supplier_bm_unit_non_bm_absvd_volume",
+                    "bm_unit_metered_volume_+ve_mwh",
+                    "bm_unit_metered_volume_-ve_mwh",
+                    "bmu_count",
+                ]
+            ]
+            .sum()
+            .to_frame()
+            .T
+        )
+        daily_total["settlement_period_count"] = len(metering_data_half_hourly)
+        daily_total.index = pd.Index([days[0]]).to_timestamp()
+        return MeteringDataDaily.from_dataframe(daily_total)
+
+
+class MeteringDataDaily(DataFrameAsset):
     schema: Dict[str, CS] = {
         **copy.deepcopy(MeteringDataHalfHourly.schema),
         "settlement_period_count": CS(check=pa.Column(int)),
     }
+
+    @classmethod
+    def transform_to_monthly(
+        cls,
+        metering_data_dataframes: List[MeteringDataDailyType],
+    ) -> MeteringDataMonthlyType:
+        """Rollup a list of daily dataframes to a single monthly dataframe"""
+        return _transform_to_monthly_or_yearly(
+            metering_data_dataframes,
+            TemporalGranularity.MONTHLY,
+            "settlement_period_count",
+            MeteringDataMonthly.from_dataframe,
+        )
 
 
 class MeteringDataMonthly(MeteringDataHalfHourly):
@@ -46,44 +92,25 @@ class MeteringDataMonthly(MeteringDataHalfHourly):
         "day_count": CS(check=pa.Column(int)),
     }
 
+    @classmethod
+    def transform_to_yearly(
+        cls,
+        metering_data_dataframes: List[MeteringDataMonthlyType],
+    ) -> MeteringDataYearlyType:
+        """Rollup a list of daily dataframes to a single monthly dataframe"""
+        return _transform_to_monthly_or_yearly(
+            metering_data_dataframes,
+            TemporalGranularity.YEARLY,
+            "day_count",
+            MeteringDataYearly.from_dataframe,
+        )
+
 
 class MeteringDataYearly(MeteringDataHalfHourly):
     schema: Dict[str, CS] = {
         **copy.deepcopy(MeteringDataHalfHourly.schema),
         "month_count": CS(check=pa.Column(int)),
     }
-
-
-def transform_to_daily(
-    metering_data_half_hourly: MeteringDataHalfHourlyType,
-) -> MeteringDataDailyType:
-    """Rollup a single, half-hourly dataframe to a daily dataframe"""
-    assert isinstance(metering_data_half_hourly.index, pd.DatetimeIndex)  # appease mypy
-    days = metering_data_half_hourly.index.to_period("D")
-    assert len(days.unique()) == 1, "Data should not span days"
-
-    daily_total = (
-        metering_data_half_hourly[
-            [
-                "period_bm_unit_balancing_services_volume",
-                "period_information_imbalance_volume",
-                "period_expected_metered_volume",
-                "bm_unit_metered_volume_mwh",
-                "bm_unit_applicable_balancing_services_volume",
-                "period_supplier_bm_unit_delivered_volume",
-                "period_supplier_bm_unit_non_bm_absvd_volume",
-                "bm_unit_metered_volume_+ve_mwh",
-                "bm_unit_metered_volume_-ve_mwh",
-                "bmu_count",
-            ]
-        ]
-        .sum()
-        .to_frame()
-        .T
-    )
-    daily_total["settlement_period_count"] = len(metering_data_half_hourly)
-    daily_total.index = pd.Index([days[0]]).to_timestamp()
-    return MeteringDataDaily.from_dataframe(daily_total)
 
 
 def _transform_to_monthly_or_yearly(
@@ -106,27 +133,3 @@ def _transform_to_monthly_or_yearly(
     output.index = output.index.to_timestamp()  # type: ignore
 
     return from_dataframe(output)
-
-
-def transform_to_monthly(
-    metering_data_dataframes: List[MeteringDataDailyType],
-) -> MeteringDataMonthlyType:
-    """Rollup a list of daily dataframes to a single monthly dataframe"""
-    return _transform_to_monthly_or_yearly(
-        metering_data_dataframes,
-        TemporalGranularity.MONTHLY,
-        "settlement_period_count",
-        MeteringDataMonthly.from_dataframe,
-    )
-
-
-def transform_to_yearly(
-    metering_data_dataframes: List[MeteringDataMonthlyType],
-) -> MeteringDataYearlyType:
-    """Rollup a list of daily dataframes to a single monthly dataframe"""
-    return _transform_to_monthly_or_yearly(
-        metering_data_dataframes,
-        TemporalGranularity.YEARLY,
-        "day_count",
-        MeteringDataYearly.from_dataframe,
-    )
