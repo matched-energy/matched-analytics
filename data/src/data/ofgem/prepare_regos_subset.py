32f2e75c5c471ad88902153c7d64abfd978607dd
Joe Kwiatkowski <joejk2@gmail.com>
2025-03-17 09:41:23 +0000

    bugfix: prepare_regos_subset

--- data/src/data/ofgem/prepare_regos_subset.py
+++ data/src/data/ofgem/prepare_regos_subset.py
@@ -1,5 +1,5 @@
+import argparse
 import copy
-import sys
 from pathlib import Path
 
 import pandas as pd
@@ -14,14 +14,33 @@ STATIONS = [
 
 
 def main(input_path: Path, output_path: Path) -> pd.DataFrame:
-    regos_df = RegosRaw(input_path).df
+    regos_df = pd.read_csv(
+        input_path,
+        header=RegosRaw.from_file_header,
+        skiprows=RegosRaw.from_file_skiprows,
+        names=list(RegosRaw.schema.keys()),
+    )
     regos_df = regos_df[regos_df["station_name"].isin(STATIONS)]
-    skip_rows = copy.deepcopy(regos_df[:4])
+    skip_rows = copy.deepcopy(regos_df[: RegosRaw.from_file_skiprows])
     skip_rows.loc[:, "station_name"] = "SKIPPED_ROW"
     regos_df = pd.concat([skip_rows, regos_df])  # mimic four rows that have to be ignored from Ofgem download
     regos_df.to_csv(output_path, header=False, index=False)
     return regos_df
 
 
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "input_path",
+        type=Path,
+    )
+    parser.add_argument(
+        "output_path",
+        type=Path,
+    )
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    main(Path(sys.argv[1]), Path(sys.argv[2]))
+    args = parse_args()
+    main(args.input_path, args.output_path)
