09e739d785f7ca55ab2c600336b31f549c17c893
Joe Kwiatkowski <joejk2@gmail.com>
2025-03-17 13:29:49 +0000

    Tidy DataFrameAsset and improve tests

--- src/ma/utils/pandas.py
+++ src/ma/utils/pandas.py
@@ -56,7 +56,7 @@ def apply_schema(
 class DataFrameAsset(ABC):
     schema: Dict[str, ColumnSchema]
     from_file_with_index: bool = True
-    from_file_skiprows: int = 0  # TODO - test
+    from_file_skiprows: int = 0
 
     def __init__(self, input: Union[pd.DataFrame, Path]):
         self._set_schema()
@@ -66,42 +66,42 @@ class DataFrameAsset(ABC):
             df = self._read_from_file(input)
         else:
             raise TypeError("Expected Pandas dataframe or pathlib.Path")
-        object.__setattr__(self, "_df", self._init_from_dataframe(df))
+        object.__setattr__(self, "_df_do_not_mutate", self._init_from_dataframe(df))
 
     def _set_schema(self) -> None:
-        self.columns: Dict = {}
-        self.index: Dict = {}
+        self._columns: Dict = {}
+        self._index: Dict = {}
         for col, column_schema in self.schema.items():
             check = column_schema["check"]
             if isinstance(check, pa.Column):
-                self.columns[col] = check
+                self._columns[col] = check
             elif isinstance(check, pa.Index):
-                if len(self.index):
+                if len(self._index):
                     raise ValueError("More than one index column defined")
-                self.index = {"check": check, "name": col}
+                self._index = {"check": check, "name": col}
             else:
                 raise ValueError("Columns must be of type pa.Column or pa.Index")
 
         self.pandera_schema = pa.DataFrameSchema(
-            columns=self.columns,
-            index=self.index.get("check"),
+            columns=self._columns,
+            index=self._index.get("check"),
             coerce=True,
             strict=True,
         )
 
     def _init_from_dataframe(self, dataframe: pd.DataFrame) -> pd.DataFrame:
-        dataframe = copy.deepcopy(dataframe)  # TODO: https://github.com/matched-energy/matched-analytics/issues/9
+        dataframe = copy.deepcopy(dataframe)
 
         # Name columns
-        column_names = pd.Index(self.columns.keys())
-        if len(self.columns) != len(dataframe.columns):
+        column_names = pd.Index(self._columns.keys())
+        if len(self._columns) != len(dataframe.columns):
             raise AssertionError(
                 f"Dataframe has wrong number of columns: expected {len(column_names)} got {len(dataframe.columns)}"
             )
         dataframe.columns = column_names
 
         # Name index
-        if index_name := self.index.get("name"):
+        if index_name := self._index.get("name"):
             dataframe.index.name = index_name
 
         # Apply schema
@@ -120,22 +120,22 @@ class DataFrameAsset(ABC):
             index_col=0 if self.from_file_with_index else None,
             skiprows=self.from_file_skiprows,
             header=None,
-        )  # TODO: Test https://github.com/matched-energy/matched-analytics/issues/9
+        )
 
     def __getattr__(self, name: str) -> Any:
-        return self._df[name]
+        return self._df_do_not_mutate[name]
 
     def __getitem__(self, key: str) -> Any:
-        return self._df[key]
+        return self._df_do_not_mutate[key]
 
     def __setattr__(self, name: str, value: Any) -> Any:
-        if name == "_df":
-            raise AttributeError(f"Cannot reassign {name}!")
+        if name == "_df_do_not_mutate":
+            raise AttributeError("Cannot reassign _df_do_not_mutate!")
         super().__setattr__(name, value)
 
     @property
     def df(self) -> pd.DataFrame:
-        return self._df.copy(deep=True)
+        return self._df_do_not_mutate.copy(deep=True)
 
     def write(self, filepath: Path) -> None:
-        self.pandera_schema.validate(self._df).to_csv(filepath)
+        self.pandera_schema.validate(self._df_do_not_mutate).to_csv(filepath)
