9a54e3e121b0b73f8231b0daa730c8220140cf4b
Joe Kwiatkowski <joejk2@gmail.com>
2025-03-20 15:46:46 +0000

    MatchMonthly uses alphabetical_renewables() + tests

--- tests/ma/matching/test_match_monthly.py
+++ tests/ma/matching/test_match_monthly.py
@@ -1,26 +1,51 @@
+from typing import Tuple
+
 import pandas as pd
 from pytest import approx
 
 import data.register
-from ma.matching.match_monthly import make_match_monthly
+from ma.matching.match_monthly import MatchMonthly, make_match_monthly
 from ma.ofgem.regos import RegosByTechMonthHolder
 from ma.upsampled_supply_hh.consumption import ConsumptionMonthly
 
 
-def test_match_monthly() -> None:
+def setup() -> Tuple[RegosByTechMonthHolder, ConsumptionMonthly, MatchMonthly]:
     supply = RegosByTechMonthHolder(data.register.REGOS_BY_TECH_MONTH_HOLDER)
     consumption = ConsumptionMonthly(data.register.CONSUMPTION_BY_MONTH)
-    match = make_match_monthly(consumption, supply).df
+    return supply, consumption, make_match_monthly(consumption, supply)
 
-    assert len(match) == 12
-    assert match["consumption_mwh"].sum() == consumption["consumption_mwh"].sum()
+
+def test_match_monthly() -> None:
+    _, consumption, match = setup()
+    match_df = match.df
+
+    assert len(match_df) == 12
+    assert match_df["consumption_mwh"].sum() == consumption["consumption_mwh"].sum()
     pd.testing.assert_series_equal(
-        match["supply_mwh_total"],
-        match.filter(like="supply_").drop(columns=["supply_mwh_total"]).sum(axis=1).rename("supply_mwh_total"),
+        match_df["supply_total_mwh"],
+        match_df.filter(regex="supply_.*_mwh")
+        .drop(columns=["supply_total_mwh", "supply_surplus_mwh", "supply_deficit_mwh"])
+        .sum(axis=1)
+        .rename("supply_total_mwh"),
     )
     pd.testing.assert_series_equal(
-        match["current_holder_count"].reset_index(drop=True),
-        pd.Series([1.0] * 12),
+        match_df["rego_holder_count"].reset_index(drop=True),
+        pd.Series([1] * 12),
         check_names=False,
     )
-    assert match["matching_score"].mean() == approx(0.672, rel=1e-3)
+    pd.testing.assert_series_equal(
+        match_df["supply_solar_mwh"].reset_index(drop=True),
+        pd.Series([0.0] * 12),
+        check_names=False,
+    )
+    pd.testing.assert_series_equal(
+        match_df["supply_solar_station_count"].reset_index(drop=True),
+        pd.Series([0] * 12),
+        check_names=False,
+    )
+    assert match_df["matching_score"].mean() == approx(0.672, rel=1e-3)
+
+
+def test_match_plot() -> None:
+    _, _, match = setup()
+    match.plot()
