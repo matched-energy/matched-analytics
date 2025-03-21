c81769d6bfce1bec8b131a94a5c92b6ca3d87089
Joe Kwiatkowski <joejk2@gmail.com>
2025-03-21 10:27:46 +0000

    Introduce _calculate_supply_surplus_deficit & _calculate_matching_score

--- tests/ma/matching/test_match_monthly.py
+++ tests/ma/matching/test_match_monthly.py
@@ -4,15 +4,31 @@ import pandas as pd
 from pytest import approx
 
 import data.register
-from ma.matching.match_monthly import MatchMonthly, MatchMonthlyAnnualised, make_match_monthly
+from ma.matching.match_monthly import (
+    MatchMonthly,
+    _calculate_matching_score,
+    _calculate_supply_surplus_deficit,
+    make_match_monthly,
+)
 from ma.ofgem.regos import RegosByTechMonthHolder
 from ma.retailer.consumption import ConsumptionMonthly
+
+
+def test_calculate_supply_surplus_deficit() -> None:
+    surplus, deficit = _calculate_supply_surplus_deficit(supply=pd.Series([0, 1, 2]), consumption=pd.Series([2, 1, 0]))
+    assert list(surplus) == [0, 0, 2]
+    assert list(deficit) == [2, 0, 0]
+
+
+def test_calculate_matching_score() -> None:
+    score = _calculate_matching_score(deficit=pd.Series([0, 1, 2]), consumption=pd.Series([2, 2, 2]))
+    assert list(score) == [1, 0.5, 0]
 
 
 def setup() -> Tuple[RegosByTechMonthHolder, ConsumptionMonthly, MatchMonthly]:
     supply = RegosByTechMonthHolder(data.register.REGOS_BY_TECH_MONTH_HOLDER)
     consumption = ConsumptionMonthly(data.register.CONSUMPTION_BY_MONTH)
-    return supply, consumption, make_match_monthly(consumption, supply)
+    return supply, consumption, make_match_monthly(consumption=consumption, supply=supply)
 
 
 def test_match_monthly() -> None:
@@ -43,6 +59,11 @@ def test_match_monthly() -> None:
         pd.Series([0] * 12),
         check_names=False,
     )
+
+    assert match_df["consumption_mwh"].sum() == approx(1257e3, rel=1e3)
+    assert match_df["supply_total_mwh"].sum() == approx(868e3, rel=1e3)
+    assert match_df["supply_deficit_mwh"].sum() == approx(394, rel=1e3)
+    assert match_df["supply_surplus_mwh"].sum() == approx(5e3, rel=1e3)
     assert match_df["matching_score"].mean() == approx(0.672, rel=1e-3)
 
 
@@ -57,7 +78,6 @@ def test_match_monthly_annualised() -> None:
     match_annualised_df = match_annualised.df
 
     assert len(match_annualised_df) == 1
-    assert list(match_annualised_df.reset_index().columns) == list(MatchMonthlyAnnualised.schema.keys())
     assert match_annualised_df["consumption_mwh"].iloc[0] == match["consumption_mwh"].sum()
     assert match_annualised_df["supply_biomass_station_max"].iloc[0] == match["supply_biomass_station_count"].max()
     assert match_annualised_df["matching_score"].iloc[0] == approx(0.6864, rel=1e-4)
