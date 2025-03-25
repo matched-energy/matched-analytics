e680eaa6830a4f91b2b756aff2701b69aab39e1d
Joe Kwiatkowski <joejk2@gmail.com>
2025-03-25 20:38:22 +0000

    Updates that allow DataFrameAsset.metadata in metering data pipeline

--- tests/ma/elexon/metering_data/test_metering_by_time.py
+++ tests/ma/elexon/metering_data/test_metering_by_time.py
@@ -30,39 +30,47 @@ def test_transforms() -> None:
     assert day_1_daily["settlement_period_count"].unique() == 48
     day_2_daily = day_2_half_hourly.transform_to_daily()
 
-    monthly = MeteringDataDaily.aggregate_to_monthly([day_1_daily.df, day_2_daily.df])
+    monthly = MeteringDataDaily.aggregate_to_monthly([day_1_daily, day_2_daily])
     assert len(monthly.df) == 1
     assert monthly["bm_unit_metered_volume_mwh"].sum() == approx(day_1_s0142_bm_mwh_sum + day_2_s0142_bm_mwh_sum)
     assert monthly["day_count"].unique() == 2
 
-    yearly = MeteringDataMonthly.aggregate_to_yearly([monthly.df])
+    yearly = MeteringDataMonthly.aggregate_to_yearly([monthly])
     assert len(yearly.df) == 1
     assert yearly["bm_unit_metered_volume_mwh"].sum() == approx(day_1_s0142_bm_mwh_sum + day_2_s0142_bm_mwh_sum)
     assert yearly["month_count"].unique() == 1
 
 
+def get_daily() -> MeteringDataDaily:
+    return (
+        ProcessedS0142(data.register.S0142_20230330_SF_20230425121906_GOLD_CSV)
+        .transform_to_half_hourly_by_bmu()
+        .transform_to_half_hourly()
+        .transform_to_daily()
+    )
+
+
 def test_transform_daily_to_monthly_assert_single_month() -> None:
+    day_1 = get_daily()
+    day_2_df = day_1.df
+    day_2_df.index = day_2_df.index + pd.DateOffset(months=1)
+    day_2 = MeteringDataDaily(day_2_df)
+
     with pytest.raises(AssertionError, match="span multiple"):
-        MeteringDataDaily.aggregate_to_monthly(
-            [
-                pd.DataFrame({"value": range(48)}, index=pd.date_range(f"2025-{m:02d}-11", periods=48, freq="30min"))
-                for m in [3, 4]
-            ]
-        )
+        MeteringDataDaily.aggregate_to_monthly([day_1, day_2])
 
 
 def test_transform_monthly_to_yearly_assert_single_year() -> None:
+    month_1 = MeteringDataMonthly(get_daily().df)
+    month_2_df = month_1.df
+    month_2_df.index = month_2_df.index + pd.DateOffset(years=1)
+    month_2 = MeteringDataMonthly(month_2_df)
     with pytest.raises(AssertionError, match="span multiple"):
-        MeteringDataMonthly.aggregate_to_yearly(
-            [
-                pd.DataFrame({"value": range(30)}, index=pd.date_range(f"{y}-03-01", periods=30, freq="D"))
-                for y in [2025, 2026]
-            ]
-        )
+        MeteringDataMonthly.aggregate_to_yearly([month_1, month_2])
 
 
 def test_transform_assert_no_duplicate_timestamps() -> None:
+    day_1 = get_daily()
+    day_2 = MeteringDataDaily(day_1.df)
     with pytest.raises(AssertionError, match="duplicate"):
-        MeteringDataMonthly.aggregate_to_yearly(
-            [pd.DataFrame({"value": range(30)}, index=pd.date_range("2025-03-01", periods=30, freq="D"))] * 2
-        )
+        MeteringDataDaily.aggregate_to_monthly([day_1, day_2])
