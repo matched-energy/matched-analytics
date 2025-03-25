dce6674b301c87109785be4861af60285e40236f
Joe Kwiatkowski <joejk2@gmail.com>
2025-03-25 20:37:00 +0000

    Introduce get_code_version()

--- src/ma/utils/misc.py
+++ src/ma/utils/misc.py
@@ -1,4 +1,13 @@
+from pathlib import Path
+
+from setuptools_scm import get_version  # type: ignore
+
+
 def truncate_string(input_string: str, max_length: int = 30, suffix: str = "...") -> str:
     if len(input_string) > max_length:
         return input_string[: max_length - len(suffix)] + suffix
     return input_string
+
+
+def get_code_version() -> str:
+    return get_version(root=Path(__file__).parent.parent.parent.parent, fallback_version="unknown")
